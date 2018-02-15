ecaaafc9b3470c20ec9b4bf38d49c3127ca4ecf1
pestretsov <pestretsov@me.com>
2018-02-16 02:59:01 +0300

    added eval implementation

--- src/Expr.ml
+++ src/Expr.ml
@@ -3,9 +3,9 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-             
-(* The type for the expression. Note, in regular OCaml there is no "@type..." 
+open GT
+
+(* The type for the expression. Note, in regular OCaml there is no "@type..."
    notation, it came from GT. *)
 @type expr =
   (* integer constant *) | Const of int
@@ -26,28 +26,51 @@ type state = string -> int
 (* Empty state: maps every variable into nothing. *)
 let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-(* Update: non-destructively "modifies" the state s by binding the variable x 
+(* Update: non-destructively "modifies" the state s by binding the variable x
    to value v and returns the new state.
 *)
 let update x v s = fun y -> if x = y then v else s y
 
-(* An example of a non-trivial state: *)                                                   
+(* An example of a non-trivial state: *)
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
 
 (* Expression evaluator
 
      val eval : state -> expr -> int
- 
-   Takes a state and an expression, andreturns the value of the expression in 
+
+   Takes a state and an expression, andreturns the value of the expression in
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+
+let rec eval s e =
+  match e with
+  | Var v -> s v
+  | Const c -> c
+  | Binop (op, e1, e2) ->
+     let l = eval s e1
+     and r = eval s e2
+     and boolToInt b = if b then 1 else 0 in
+     match op with
+     | "+"  -> l + r
+     | "-"  -> l - r
+     | "*"  -> l * r
+     | "/"  -> l / r
+     | "%"  -> l mod r
+     | "<"  -> boolToInt (l < r)
+     | ">"  -> boolToInt (l > r)
+     | ">=" -> boolToInt (l >= r)
+     | "<=" -> boolToInt (l <= r)
+     | "==" -> boolToInt (l = r)
+     | "!=" -> boolToInt (l <> r)
+     | "&&" -> boolToInt (l <> 0 && r <> 0)
+     | "!!" -> boolToInt (l <> 0 || r <> 0)
+     | _    -> failwith "this operation is not supported"
